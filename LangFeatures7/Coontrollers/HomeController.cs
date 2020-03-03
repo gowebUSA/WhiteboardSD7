602c3f633a76d7bdf1c1ab36c1b86f2b83c981cb
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-03-03 12:12:52 -0800

    Created Language Features from the book.

---
+++ LangFeatures7/Coontrollers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LangFeatures7.Models;
+
+namespace LangFeatures7.Coontrollers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Dictionary<string, Product> products =
+                new Dictionary<string, Product>
+                {
+                    {"kayak", new Product{Name="Kayak", Price=375M} },
+                    {"lifejaket", new Product{ Name="PFD", Price=50.95M } }
+                };
+            return View("Index", products.Keys);
+        }
+        public ViewResult Index2()
+        {
+            //return View(new string[] { "C#", "Language", "Features" });
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;    // ? nullable
+                decimal? price = p?.Price;
+                string relatedItem = p?.Related?.Name ?? "<None>";         // ?? null coalescing operator
+                results.Add(string.Format(
+                    $"Name: {name}," +              //We use sring interppoaltion instead of using array positioning.
+                    $"Price: {price}," + 
+                    $"Related : {relatedItem}"));       //, name, price, relatedItem
+            }
+            return View(results);
+        }
+        public ViewResult Index3()
+        {
+            string[] names = new string[3];
+            names[0] = "Bob";
+            names[1] = "Joe";
+            names[2] = "Alice";
+            return View("Index", names);
+        }
+    }
+}
--- LangFeatures7/Startup.cs
+++ LangFeatures7/Startup.cs
@@ -16,25 +16,30 @@ namespace LangFeatures7
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+                            //> I is interface
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //webpage that is return when there is an exception. Maybe I don't want to show end user  except developer.
             }
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("LangFeatures7");
+            //    });
+            //});
+            
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
