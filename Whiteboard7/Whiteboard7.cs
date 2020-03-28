31014ef56cca937cc86ba859181d9a044f4ec0ef
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-03-27 22:28:46 -0700

    Tranfers projects to MssaHwSd7 Solution.

--- Whiteboard7/Whiteboard7.cs
+++ Whiteboard7/Whiteboard7.cs
@@ -7,18 +7,14 @@ namespace Whiteboard7
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tWhiteboard7.Program.Main()");
-            //200203. Calculating Averages, if statements, .Parse
-            //200204. Tuition has the compound interest formula.
-            //200206. Animal Sound, Overloading
-            //200211. Calendar, 
+            //200203. CalculatingAverages, if statements, .Parse
+            //200204. Tuition has the compound interest formula. *************************************************
+            //200211. Calendar, was unloaded.
             //200214. Alphabetize ToLower()
             //ASP>NET MVC Starts..................................................................................
             //200309. LinqDemoCode
             //200304. ClockDegreeAngle, .Abs, (int)(value)
-            //200305. LangFeatures7. ASP Exercise. Chapter 4.
-            //200307. Game Roulette, if, switch with goto, 
             //200310. CountCapsLetter, IsUpper()
-            //200310. RazorSD7. ASP Exercise. Week 9. Chapter 5. LAB9B.
             //200311. WorkingWithVS7. ASP Exercise. Week 9. Chapter 6. LAB9C. NuGet, bootstrap, debugger, BrowserLink.
                       //Also see the EX fle on C:\Users\goric\OneDrive\Documents\MSSA\421 Developing Cloud App\..
                       //..Lab\Week 10\LAB 1A WorkingWithVisualStudio\WorkingWithVisualStudio.
