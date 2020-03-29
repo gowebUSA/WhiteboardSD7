d7630ae8b24c217f3ff0e95ba96c8afd0be43928
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-03-28 19:27:31 -0700

    Organizes Repositories: Whiteboard7 and MssaHwSd7.

--- Whiteboard7/Whiteboard7.cs
+++ Whiteboard7/Whiteboard7.cs
@@ -7,8 +7,9 @@ namespace Whiteboard7
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tWhiteboard7.Program.Main()");
+            //200127. XYZ. Add Two Numbers.
             //200203. CalculatingAverages, if statements, .Parse
-            //200204. Tuition has the compound interest formula. *************************************************
+            //200204. Tuition has the compound interest formula. ***Transfers to MssaHwSd7 on 200328***
             //200211. Calendar, was unloaded.
             //200214. Alphabetize ToLower()
             //ASP>NET MVC Starts..................................................................................
@@ -16,12 +17,15 @@ namespace Whiteboard7
             //200304. ClockDegreeAngle, .Abs, (int)(value)
             //200310. CountCapsLetter, IsUpper()
             //200311. WorkingWithVS7. ASP Exercise. Week 9. Chapter 6. LAB9C. NuGet, bootstrap, debugger, BrowserLink.
-                      //Also see the EX fle on C:\Users\goric\OneDrive\Documents\MSSA\421 Developing Cloud App\..
-                      //..Lab\Week 10\LAB 1A WorkingWithVisualStudio\WorkingWithVisualStudio.
+            //Also see the EX fle on C:\Users\goric\OneDrive\Documents\MSSA\421 Developing Cloud App\..
+            //..Lab\Week 10\LAB 1A WorkingWithVisualStudio\WorkingWithVisualStudio.
             //200324. SportStoreSD7.  ASP.NET Chap 8. 
             //200324. SportStoreSD7.Test. Using xUnit Test created from Whiteboard7 Solution.
             //200326. ContosoUniversityData7. Creates SQL file. docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/database-first-development/setting-up-database
             //200326. ContoseSite7. Connects SQL, DO.NET, EntityFramework v6.
+            //200327. WorkingWithVS7 + .Tests with Unit Testing, Fake Repository and Moq. 
+            //200328. Transfers WorkingWithVS7 + .Tests to MssaHwSd7 Solution.
+            //200328. Transfers ContosoSite7 + ContosoUniversityData7 from Whiteboard7 Solution.
         }
     }
 }
